6b59c4cb712ef4d947f78f9369ebec390fa720a7
farzadsohrabii <166904535+farzadsohrabii@users.noreply.github.com>
2025-12-25 11:12:58 +0330

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,5 @@
 import PageTransition from "@/components/pagetransition";
 import Link from "next/link";
-import { Lexend } from 'next/font/google'
-import { Geist  } from 'next/font/google' 
 import ProjectCard from "@/components/projectCard";
 
 
@@ -35,7 +33,7 @@ export default function Projects() {;
                     <h2 className="lg:text-[#784ce5] lg:font-semibold lg:mb-2 ---------- font-bold text-[22px]">My <span className="lg:text-[#784ce5] ---------- inline text-[#61c3b6]">Projects</span></h2>
                 </div>
                 {/* the projects box */}
-                <div className="lg:flex lg:flex-row gap-6  justify-center w-[345.6px]">
+                <div className="lg:flex lg:flex-row w-full gap-6  justify-center">
                     <div>
                         <ProjectCard buttonColor="hover:bg-[#61c3b6]" bordercolor="border-b-[#61c3b6]/30" image="/PortfolioProje.png" name="Portfolio Website" description="My personal portfolio website with a responsive layout and modern UI" tag1="Next.js" tag2="TypeScript"tag3="Tailwind CSS" link="https://farzadsohrabi.me"/>
                     </div>
